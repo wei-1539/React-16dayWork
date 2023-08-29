6f5cc4c4cb056caa1a9452b483b23383a85aabc7
startoasia <sta_rt@startoasia.com>
2023-08-29 17:07:35 +0800

    每日任務16 React Router 建立

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css';
+import { HashRouter, NavLink, Route, Routes } from 'react-router-dom';
+
+const Todo = () => {
+  return <p>這是 Todo 頁面</p>;
+};
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+function App() {
+  return (
+    <div className="container">
+      <HashRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+        </div>
+        {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/todo' element={<Todo />} />
+          {/* 不存在的路由 */}
+          <Route path='*' element={
+            <main>
+               <p>There's nothing here!</p>
+            </main>
+          }/>
+        </Routes>
+        {/* 練習區 */}
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
